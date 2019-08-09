1f8baac67faca14a3ffbd49b5e5d841b79475fac
Simon Hofmann <simon.hofmann@consol.de>
2019-08-09 14:28:28 +0200

    checkmk implementation

--- packages/sakuli-forwarder-check_mk/src/checkmk-forwarder-properties.class.ts
+++ packages/sakuli-forwarder-check_mk/src/checkmk-forwarder-properties.class.ts
@@ -8,7 +8,7 @@ export class CheckMkForwarderProperties {
      */
     @BooleanProperty("sakuli.forwarder.check_mk.enabled")
     @IsBoolean()
-    enabled: boolean = false
+    enabled: boolean = false;
 
     /**
      * spool dir, default /var/lib/check_mk_agent/spool (Linux) / (installation_path)\spool (Windows)
@@ -28,14 +28,18 @@ export class CheckMkForwarderProperties {
      * Prefix of the file name for CheckMK
      */
     @StringProperty("sakuli.forwarder.check_mk.spoolfile_prefix")
-    @IsNotEmpty()
-    spoolfilePrefix: string ="sakuli_suite_";
+    spoolfilePrefix: string = "sakuli_suite";
 
     /**
      * optional service description forwarded to the output check result, when not set, testsuite.id is used
      */
     @StringProperty("sakuli.forwarder.check_mk.service_description")
-    @IsNotEmpty()
-    serviceDescription:string = "${testsuite.id}";
+    serviceDescription: string = "";
 
+    /**
+     * Boolean property to disable detailed summary in check results
+     * DEFAULT: true
+     */
+    @BooleanProperty("sakuli.forwarder.check_mk.details")
+    outputDetails: boolean = true;
 }
