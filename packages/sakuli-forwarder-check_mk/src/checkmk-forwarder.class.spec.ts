5eeb7c29597320078efc691cfeec238f1ca69f02
Julian Liebich <Julian.Liebich@consol.de>
2020-07-02 14:40:06 +0200

    [#53] Added Test for conditional validation of forwarder properties.

---
+++ packages/sakuli-forwarder-check_mk/src/checkmk-forwarder.class.spec.ts
@@ -0,0 +1,71 @@
+import { CheckMkForwarder } from "./checkmk-forwarder.class";
+import { Project, TestExecutionContext } from "@sakuli/core";
+import { mockPartial } from "sneer";
+import { SimpleLogger } from "@sakuli/commons";
+import { validateProps } from "@sakuli/result-builder-commons";
+
+jest.mock("@sakuli/result-builder-commons", () => ({
+  validateProps: jest.fn(),
+}));
+describe("check-mk forwarder", () => {
+
+  let checkmkForwarder: CheckMkForwarder;
+  let context: TestExecutionContext;
+
+  const logger = mockPartial<SimpleLogger>({
+    info: jest.fn(),
+    debug: jest.fn()
+  });
+
+  function getProjectWithProps(props: any){
+    return mockPartial<Project>({
+      has(key: string): boolean {
+        return props[key] !== undefined;
+      },
+      get(key: string): any {
+        return props[key];
+      }
+    })
+  }
+
+  beforeEach(() => {
+    checkmkForwarder = new CheckMkForwarder();
+    context = new TestExecutionContext(logger);
+    jest.clearAllMocks();
+  });
+
+  it("should not validate props if not available", () => {
+    // GIVEN
+    const project = getProjectWithProps({});
+
+    //WHEN
+    checkmkForwarder.setup(project, logger);
+
+    //THEN
+    expect(validateProps).not.toHaveBeenCalled();
+  });
+
+  it("should validate props if available", () => {
+    //GIVEN
+    const project = getProjectWithProps({
+    "sakuli.forwarder.check_mk.enabled" : "true"
+    });
+
+    //WHEN
+    checkmkForwarder.setup(project,logger);
+
+    //THEN
+    expect(validateProps).toHaveBeenCalled();
+  });
+
+  it("should throw error when props are invalid", () => {
+    //GIVEN
+    const project = getProjectWithProps({
+    "sakuli.forwarder.check_mk.enabled" : "123"
+    });
+
+    //THEN
+    expect(checkmkForwarder.setup(project,logger)).rejects.toThrow();
+  });
+
+});
