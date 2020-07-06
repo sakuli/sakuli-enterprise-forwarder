bf164b6ddbe1314e60d4d1c3387c5cecfa5176aa
Van Cliff Le <cliff.le.van@consol.de>
2020-07-06 12:48:03 +0200

    (sakuli/sakuli#338) refactoring

--- packages/sakuli-forwarder-check_mk/src/checkmk-forwarder.class.spec.ts
+++ packages/sakuli-forwarder-check_mk/src/checkmk-forwarder.class.spec.ts
@@ -3,23 +3,18 @@ import { SimpleLogger } from "@sakuli/commons";
 import { mockPartial } from "sneer";
 import { CheckMkForwarder } from "./checkmk-forwarder.class";
 import { dirExists } from "./dir-exists.function";
-import {promises as fs} from "fs";
+import { promises as fs } from "fs";
 import { cwd } from "process";
 
 jest.mock("./create-spool-file.function", () => ({
-    createSpoolFileName : jest.fn(() => { return "spoolFileName" })
+  createSpoolFileName: jest.fn(() => {
+    return "spoolFileName"
+  })
 }));
 
 jest.mock("./dir-exists.function", () => ({
-    dirExists : jest.fn()
-        .mockImplementationOnce(() => true)
-        .mockImplementationOnce(() => false)
-        .mockImplementationOnce(() => true)
+  dirExists: jest.fn()
 }));
-
-fs.writeFile = jest.fn()
-    .mockImplementationOnce(() => {})
-    .mockImplementationOnce(() => { throw new Error(); });
 
 describe("checkmk forwarder", () => {
   let context: TestExecutionContext;
@@ -32,7 +27,12 @@ describe("checkmk forwarder", () => {
     error: jest.fn(),
   });
 
-  function getProjectWithProps(props: any){
+  const defaultProject = getProjectWithProps({
+    "sakuli.forwarder.check_mk.enabled": true,
+    "sakuli.forwarder.check_mk.spooldir": "spoolFilePath"
+  });
+
+  function getProjectWithProps(props: any) {
     return mockPartial<Project>({
       has(key: string): boolean {
         return props[key] !== undefined;
@@ -43,11 +43,20 @@ describe("checkmk forwarder", () => {
     })
   }
 
-  function endContext(ctx: TestExecutionContext){
-      ctx.endTestStep();
-      ctx.endTestCase();
-      ctx.endTestSuite();
-      ctx.endExecution();
+  async function setupDefaultProject() {
+    await checkMkForwarder.setup(defaultProject, logger);
+    context.startExecution();
+    context.startTestSuite({id: 'Suite1'});
+    context.startTestCase({id: 'Suite1Case1'});
+    context.startTestStep({id: 'Suite1Case1Step1'});
+    endContext(context);
+  }
+
+  function endContext(ctx: TestExecutionContext) {
+    ctx.endTestStep();
+    ctx.endTestCase();
+    ctx.endTestSuite();
+    ctx.endExecution();
   }
 
   beforeEach(() => {
@@ -60,12 +69,12 @@ describe("checkmk forwarder", () => {
     await expect(checkMkForwarder.forward(context)).rejects.toThrowError();
   });
 
-  it("should give depub message when props disabled", async () => {
+  it("should give debug message when props disabled", async () => {
     //GIVEN
     const project = getProjectWithProps({
-        "sakuli.forwarder.check_mk.enabled" : false
+      "sakuli.forwarder.check_mk.enabled": false
     });
-    await checkMkForwarder.setup(project,logger);
+    await checkMkForwarder.setup(project, logger);
 
     //WHEN
     await checkMkForwarder.forward(context);
@@ -76,76 +85,49 @@ describe("checkmk forwarder", () => {
   });
 
   it("should resolve when properties are present", async () => {
+    //GIVEN
+    (<jest.Mock>dirExists).mockReturnValueOnce(true);
+    fs.writeFile = jest.fn()
+      .mockImplementationOnce(() => {});
+    await setupDefaultProject();
 
-      //GIVEN
-      const project = getProjectWithProps({
-          "sakuli.forwarder.check_mk.enabled" : true,
-          "sakuli.forwarder.check_mk.spooldir" : "spoolFilePath"
-      });
-      await checkMkForwarder.setup(project,logger);
-      context.startExecution();
-      context.startTestSuite({id: 'Suite1'});
-      context.startTestCase({id: 'Suite1Case1'});
-      context.startTestStep({id: 'Suite1Case1Step1'});
-      endContext(context);
+    //WHEN
+    await checkMkForwarder.forward(context);
 
-      //WHEN
-      await checkMkForwarder.forward(context);
-
-      //THEN
-      expect(logger.info).toBeCalledWith(`Forwarding final result to checkmk via spool file 'spoolFileName' in 'spoolFilePath'.`);
-      expect(dirExists).toHaveBeenCalled();
-      expect(fs.writeFile).toHaveBeenCalled();
-    });
+    //THEN
+    expect(logger.info).toBeCalledWith(`Forwarding final result to checkmk via spool file 'spoolFileName' in 'spoolFilePath'.`);
+    expect(fs.writeFile).toHaveBeenCalled();
+  });
 
   it("should warn when spool directory does not exist", async () => {
 
-      //GIVEN
-      const project = getProjectWithProps({
-          "sakuli.forwarder.check_mk.enabled" : true,
-          "sakuli.forwarder.check_mk.spooldir" : "spoolFilePath"
-      });
-      await checkMkForwarder.setup(project,logger);
-      context.startExecution();
-      context.startTestSuite({id: 'Suite1'});
-      context.startTestCase({id: 'Suite1Case1'});
-      context.startTestStep({id: 'Suite1Case1Step1'});
-      endContext(context);
+    //GIVEN
+    (<jest.Mock>dirExists).mockReturnValueOnce(false);
+    await setupDefaultProject()
+    //WHEN
+    await checkMkForwarder.forward(context);
 
-      //WHEN
-      await checkMkForwarder.forward(context);
-
-      //THEN
-      expect(logger.info).toBeCalledWith(`Forwarding final result to checkmk via spool file 'spoolFileName' in 'spoolFilePath'.`);
-      expect(dirExists).toHaveBeenCalled();
-      expect(logger.warn).toHaveBeenCalledWith(`spool directory 'spoolFilePath' does not exists, skipping checkmk forwarding.`);
-      
+    //THEN
+    expect(logger.info).toBeCalledWith(`Forwarding final result to checkmk via spool file 'spoolFileName' in 'spoolFilePath'.`);
+    expect(logger.warn).toHaveBeenCalledWith(`spool directory 'spoolFilePath' does not exists, skipping checkmk forwarding.`);
   });
 
   it("should throw an error if writing file does fail", async () => {
 
-      //GIVEN
-      const errorPath = `${cwd()}/spoolFilePath/spoolFileName`;
-      const project = getProjectWithProps({
-          "sakuli.forwarder.check_mk.enabled" : true,
-          "sakuli.forwarder.check_mk.spooldir" : "spoolFilePath"
+    //GIVEN
+    (<jest.Mock>dirExists).mockReturnValueOnce(true);
+    fs.writeFile = jest.fn()
+      .mockImplementationOnce(() => {
+        throw Error();
       });
-      await checkMkForwarder.setup(project,logger);
-      context.startExecution();
-      context.startTestSuite({id: 'Suite1'});
-      context.startTestCase({id: 'Suite1Case1'});
-      context.startTestStep({id: 'Suite1Case1Step1'});
-      endContext(context);
+    const errorPath = `${cwd()}/spoolFilePath/spoolFileName`;
+    await setupDefaultProject();
 
-      //WHEN
-      await checkMkForwarder.forward(context);
+    //WHEN
+    await checkMkForwarder.forward(context);
 
-      //THEN
-      expect(logger.info).toBeCalledWith(`Forwarding final result to checkmk via spool file 'spoolFileName' in 'spoolFilePath'.`);
-      expect(dirExists).toHaveBeenCalled();
-      expect(fs.writeFile).toHaveBeenCalled();
-      expect(logger.error).toHaveBeenCalledWith(`Failed to write to '${errorPath}'. Reason:`, new Error());
-    });
-
+    //THEN
+    expect(logger.error).toHaveBeenCalledWith(`Failed to write to '${errorPath}'. Reason:`, new Error());
+  });
 
 });
