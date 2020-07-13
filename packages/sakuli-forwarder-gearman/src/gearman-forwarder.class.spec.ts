60f7fe7c970214cee9c63456c815240902a4d3f8
MarleneSahra <marlenek@consol.de>
2020-07-13 17:41:53 +0200

    (sakuli/sakuli#338) added tests for changed log level

--- packages/sakuli-forwarder-gearman/src/gearman-forwarder.class.spec.ts
+++ packages/sakuli-forwarder-gearman/src/gearman-forwarder.class.spec.ts
@@ -3,14 +3,27 @@ import { Project, TestExecutionContext } from "@sakuli/core";
 import { mockPartial } from "sneer";
 import { SimpleLogger } from "@sakuli/commons";
 import { validateProps } from "@sakuli/result-builder-commons";
+import { TestSuiteContext, FinishedMeasurable, TestCaseContext } from "@sakuli/core";
 
-jest.mock("@sakuli/result-builder-commons", () => ({
-  validateProps: jest.fn(),
-}));
+
+jest.mock("@sakuli/result-builder-commons", () => {
+    const originalModule = jest.requireActual("@sakuli/result-builder-commons");
+
+    return {
+        __esModule: true,
+        ...originalModule,
+        validateProps: jest.fn(),
+    };
+});
+
 describe("gearman forwarder", () => {
 
   let gearmanForwarder: GearmanForwarder;
   let context: TestExecutionContext;
+  let testEntity: Partial<TestCaseContext & FinishedMeasurable>;
+  let suiteEntity: Partial<TestSuiteContext & FinishedMeasurable>;
+  let testCaseEntity: TestCaseContext & FinishedMeasurable;
+  let testSuiteEntity: TestSuiteContext & FinishedMeasurable;
 
   const logger = mockPartial<SimpleLogger>({
     info: jest.fn(),
@@ -31,6 +44,10 @@ describe("gearman forwarder", () => {
   beforeEach(() => {
     gearmanForwarder = new GearmanForwarder();
     context = new TestExecutionContext(logger);
+    suiteEntity = {}
+    testEntity = {};
+    testCaseEntity = testEntity as TestCaseContext & FinishedMeasurable;
+    testSuiteEntity = suiteEntity as TestSuiteContext & FinishedMeasurable;
     jest.clearAllMocks();
   });
 
@@ -53,9 +70,28 @@ describe("gearman forwarder", () => {
 
     //WHEN
     await gearmanForwarder.setup(project,logger);
+    //await gearmanForwarder.forwardCaseResult(testCaseEntity,context);
+
 
     //THEN
     expect(validateProps).toHaveBeenCalled();
+    //expect(logger.info).toHaveBeenCalledWith("Forwarding case result.")
+
+  });
+
+  it("should log the right info after calling forwardSuiteResult", async () => {
+    //GIVEN
+    const project = getProjectWithProps({
+    "sakuli.forwarder.gearman.enabled" : "true"
+    });
+
+    //WHEN
+    await gearmanForwarder.setup(project,logger);
+    await gearmanForwarder.forwardSuiteResult(testSuiteEntity,context);
+
+    //THEN
+    expect(logger.info).toHaveBeenCalledWith("Forwarding suite result.");
+
   });
 
 });
