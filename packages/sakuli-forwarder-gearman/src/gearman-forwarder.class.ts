383fe64f993206a0614f2140219728a5ec6be34f
MarleneSahra <marlenek@consol.de>
2020-07-14 16:01:23 +0200

    (sakuli/sakuli#338) prototypical repair of missing parentSuite

--- packages/sakuli-forwarder-gearman/src/gearman-forwarder.class.ts
+++ packages/sakuli-forwarder-gearman/src/gearman-forwarder.class.ts
@@ -117,8 +117,12 @@ export class GearmanForwarder implements Forwarder {
             if (properties.enabled) {
                 const parentCase: Maybe<TestCaseContext> = ctx.testCases.find(tc => tc.getChildren().includes(entity));
                 const parentSuite: Maybe<TestSuiteContext> = ifPresent(parentCase, (testCase) => {
-                    return ctx.testSuites.find(ts => ts.getChildren().includes(testCase));
-                }, () => undefined);
+                    return ctx.testSuites.find(ts =>
+                              ts.getChildren().find((element) =>
+                                      (testCase.id === element.id)
+                              ) !== undefined);
+                }, () =>{
+                return undefined});
                 ifPresent(parentSuite, async (suite) => {
                     const renderedTemplate = this.outputBuilder.render(entity, {
                         currentSuite: suite,
